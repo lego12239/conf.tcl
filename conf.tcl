5e656aca30634199a972678d2d313e8e1ebeab7f
Oleg Nemanov <lego12239@yandex.ru>
2022-10-12 18:00:42 +0300

    change copyright date range

--- conf.tcl
+++ conf.tcl
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Oleg Nemanov <lego12239@yandex.ru>
+# Copyright (c) 2020-2022 Oleg Nemanov <lego12239@yandex.ru>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
