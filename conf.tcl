ac28d39d5b3b08998fa3fe9099eeb3cc59059c9d
Oleg Nemanov <lego12239@yandex.ru>
2022-10-31 18:57:39 +0300

    remove _load proc

--- conf.tcl
+++ conf.tcl
@@ -54,7 +54,7 @@ proc load_from_file {args} {
 	_ctx_src_push ctx $src
 
 	set err ""
-	if {[catch {_load ctx} conf]} {
+	if {[catch {_parse ctx [dict get $ctx prms -default]} conf]} {
 		set err [list $conf $::errorInfo $::errorCode]
 	}
 
@@ -103,7 +103,7 @@ proc load_from_fh {args} {
 
 	set ctx [_ctx_mk $opts]
 	_ctx_src_push ctx $src
-	set conf [_load ctx]
+	set conf [_parse ctx [dict get $ctx prms -default]]
 	_ctx_src_pop ctx
 
 	if {[dict exists $opts -cb]} {
@@ -157,7 +157,7 @@ proc load_from_str {args} {
 	set ctx [_ctx_mk $opts]
 	_ctx_src_push ctx $src
 #	puts "CTX: $ctx"
-	set conf [_load ctx]
+	set conf [_parse ctx [dict get $ctx prms -default]]
 	_ctx_src_pop ctx
 
 	if {[dict exists $opts -cb]} {
@@ -206,14 +206,6 @@ proc _opts_parse {argslist spec} {
 	}
 
 	return [list $opts $i]
-}
-
-# Start a parsing.
-# prms:
-#  ctx - context
-proc _load {_ctx} {
-	upvar $_ctx ctx
-	return [_parse ctx [dict get $ctx prms -default]]
 }
 
 proc _parse {_ctx conf} {
